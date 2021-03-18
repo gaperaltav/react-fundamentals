7ac2bb79e1f053fdde1b3e0b7cb4a8208c483bf0
Gabriel Peralta <gaperaltav@gmail.com>
2021-03-18 01:22:02 -0400

    Exercise no.5 competing extracredit without video --formating

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -15,15 +15,30 @@ import '../box-styles.css'
 
 function Box({className, style, size, children}) {
   return (
-    <div className={`box box--${size} ${className}`} style={{ fontStyle: 'italic', ...style}}>
+    <div
+      className={`box box--${size} ${className}`}
+      style={{fontStyle: 'italic', ...style}}
+    >
       {children}
     </div>
   )
 }
 
-const smallBox = <Box size="small" style={{backgroundColor: 'lightblue'}} > small lightblue box</Box>
-const mediumBox = <Box size="medium" style={{backgroundColor: 'pink'}}>medium pink box</Box>
-const largeBox = <Box size="large" style={{backgroundColor: 'orange'}}>large orange box</Box>
+const smallBox = (
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
+    small lightblue box
+  </Box>
+)
+const mediumBox = (
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
+    medium pink box
+  </Box>
+)
+const largeBox = (
+  <Box size="large" style={{backgroundColor: 'orange'}}>
+    large orange box
+  </Box>
+)
 
 function App() {
   return (
